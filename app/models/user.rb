5f2469e8923747f9ba20c31eec7c6de8038e5fa7
sorafujiyama <fs19970202@gmail.com>
2020-09-17 14:54:50 +0900

    ログインしているユーザーだけが、出品ページへ遷移できること

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
+  has_many :items
 end
 
 
