f9894fc1a87a84be948ffa06a26cc0acccfb0cf8
sorafujiyama <fs19970202@gmail.com>
2020-09-17 17:07:46 +0900

    バリデーションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,14 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :decription, :category, :status, :shopping_cost, :shopping_area, :shopping_days, :price).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :decription, presence: true
+  validates :category, presence: true
+  validates :status, presence: true
+  validates :shopping_cost, presence: true
+  validates :shopping_area, presence: true
+  validates :shopping_days, presence: true
+  validates :price, presence: true
+
   belongs_to :user
+  has_one_attached :image
 end
