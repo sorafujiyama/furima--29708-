daed32210ffa14c8b7fdb31c4d95970977c81c82
sorafujiyama <fs19970202@gmail.com>
2020-09-18 18:32:55 +0900

    商品出品機能の修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -24,19 +24,19 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Decription can't be blank")
       end
       it "カテゴリーが空だと出品できない" do
-        @item.category = nil
+        @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category is not a number")
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it "商品の状態が空だと出品できない" do
-        @item.status = nil
+        @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status is not a number")
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it "配送料の負担が空だと出品ができない" do
-        @item.shopping_cost = nil
+        @item.shopping_cost_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shopping cost is not a number")
+        expect(@item.errors.full_messages).to include("Shopping cost must be other than 1")
       end
       it "発送元の地域が空だと出品できない" do
         @item.shopping_area = nil
