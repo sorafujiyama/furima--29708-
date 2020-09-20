8ff3fdb902fec69e232682618ce986fd2a7d608f
sorafujiyama <fs19970202@gmail.com>
2020-09-20 17:51:11 +0900

    商品詳細ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @shopping_day = ShoppingDay.find(@item.shopping_days_id)
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :decription, :category_id, :status_id, :shopping_cost_id, :shopping_area_id, :shopping_days_id, :price).merge(user_id: current_user.id)
