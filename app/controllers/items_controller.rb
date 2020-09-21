8bea6eedfcc482ebebb97a4bc1f017e4fd3c5849
sorafujiyama <fs19970202@gmail.com>
2020-09-21 16:34:28 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,21 @@ class ItemsController < ApplicationController
     @shopping_day = ShoppingDay.find(@item.shopping_days_id)
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :decription, :category_id, :status_id, :shopping_cost_id, :shopping_area_id, :shopping_days_id, :price).merge(user_id: current_user.id)
