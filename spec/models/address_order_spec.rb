db5845d8336ead598109e1ba3e565998a5a78342
sorafujiyama <fs19970202@gmail.com>
2020-09-27 10:39:46 +0900

    商品購入機能の修正２

--- spec/models/address_order_spec.rb
+++ spec/models/address_order_spec.rb
@@ -15,17 +15,16 @@ RSpec.describe AddressOrder, type: :model do
       it "建物名以外が存在すれば購入できる" do
         expect(@address_order).to be_valid
       end
+      it "電話番号はハイフンは不要で、11桁以内だと購入できる" do
+        @address_order.tel = "09012345678"
+        expect(@address_order).to be_valid
+      end
     end
 
     context "商品購入がうまくいかないとき" do
       
       it "郵便番号が空だと購入できない" do
         @address_order.postal_code = nil
-        @address_order.valid?
-        expect(@address_order.errors.full_messages).to include("Postal code is invalid")
-      end
-      it "郵便番号が11桁以上だと購入できない" do
-        @address_order.postal_code = "123456789123"
         @address_order.valid?
         expect(@address_order.errors.full_messages).to include("Postal code is invalid")
       end
@@ -53,6 +52,11 @@ RSpec.describe AddressOrder, type: :model do
         @address_order.tel = nil
         @address_order.valid?
         expect(@address_order.errors.full_messages).to include("Tel can't be blank", "Tel is invalid")
+      end
+      it "電話番号が11桁以上だと購入できない" do
+        @address_order.tel = "123456789123"
+        @address_order.valid?
+        expect(@address_order.errors.full_messages).to include("Tel is invalid")
       end
     end
   end
